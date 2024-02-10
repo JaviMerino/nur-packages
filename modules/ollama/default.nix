1b6e98af22d342ea485a372fe35941604cac5014
Javi Merino <merino.jav@gmail.com>
2024-02-10 14:22:19 +0000

    ollama: format with nixfmt

--- modules/ollama/default.nix
+++ modules/ollama/default.nix
@@ -1,26 +1,21 @@
 { config, lib, pkgs, ... }:
-let
-  cfg = config.services.ollama;
-in
-{
+
+let cfg = config.services.ollama;
+in {
   options.services.ollama = {
-    enable = lib.mkEnableOption "ollama server";
-    package = lib.mkPackageOption pkgs "ollama" { } ;
+    enable = lib.mkEnableOption "ollama systemd service";
+    package = lib.mkPackageOption pkgs "ollama" { };
   };
 
   config = lib.mkIf cfg.enable {
     systemd.user.services.ollama = {
-      Unit = {
-        Description = "ollama server";
-      };
+      Unit.Description = "ollama server";
       Service = {
         Type = "exec";
         ExecStart = "${cfg.package}/bin/ollama serve";
         Restart = "on-failure";
       };
-      Install = {
-        WantedBy = ["default.target"];
-      };
+      Install.WantedBy = [ "default.target" ];
     };
   };
 }
