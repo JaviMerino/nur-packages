e8d5b5b287dfe0abfd4523977ce81b17fb184e4f
Javi Merino <merino.jav@gmail.com>
2024-02-11 18:06:19 +0000

    koji: enable tests

--- pkgs/koji/default.nix
+++ pkgs/koji/default.nix
@@ -1,10 +1,13 @@
-{ fetchFromGitHub
+{ coreutils
+, fetchFromGitHub
 , fetchzip
+, glibcLocales
 , lib
 , python3Packages
   #, rpm
 , python-requests-gssapi
 , system
+, tzdata
 }:
 
 python3Packages.buildPythonApplication rec {
@@ -14,6 +17,10 @@ python3Packages.buildPythonApplication rec {
     url = "https://pagure.io/koji/archive/koji-${version}/koji-koji-${version}.tar.gz";
     hash = "sha256-pUAfg/wS8Wxzj9Udhr/R+QEQvQSdRr8yGFqbDR7pfT0=";
   };
+  patches = [
+    ./test_load_plugins-mock_expand_user.patch
+  ];
+
   propagatedBuildInputs = with python3Packages; [
     dateutil
     psycopg2
@@ -23,8 +30,32 @@ python3Packages.buildPythonApplication rec {
     six
   ];
 
-  # TODO enable only the cli tests
-  doCheck = false;
+  preCheck = ''
+    substituteInPlace tests/test_cli/test_import_sig.py --replace /bin/ls "${coreutils}/bin/ls"
+
+    # The test harness tries to set the timezone, so it needs access to tzdata's zoneinfo
+    export TZDIR=${tzdata}/share/zoneinfo
+  '';
+  postCheck = ''
+    unset TZDIR
+  '';
+
+  checkInputs = (with python3Packages; [
+    mock
+    pytest
+    requests-mock
+  ]) ++ [
+    glibcLocales
+  ];
+  setuptoolsCheckPhase = ''
+    runHook preCheck
+
+    # The only tests suites that are relevant for the cli we are building
+    python3 setup.py test --test-suite tests.test_cli
+    python3 setup.py test --test-suite tests.test_lib
+
+    runHook postCheck
+  '';
 
   meta = with lib; {
     description = "A flexible, secure, and reproducible way to build RPM-based software.";
