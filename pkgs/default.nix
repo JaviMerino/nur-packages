ab1d7ce5732503a0f78e6f2ead45956ba955d649
Javi Merino <merino.jav@gmail.com>
2024-02-13 20:49:44 +0000

    python-ollama: init at 0.1.6

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -7,6 +7,7 @@ in
   ack-results-parser = callPackage ./ack-results-parser { };
   pybeam = callPackage ./pybeam { };
   python-hwinfo = callPackage ./python-hwinfo { };
+  python-ollama = callPackage ./python-ollama { };
   python-qpid-proton = callPackage ./python-qpid-proton { };
   rpmlint = callPackage ./rpmlint { };
 })
