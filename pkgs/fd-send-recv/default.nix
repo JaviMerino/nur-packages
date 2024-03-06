af51b13c2408e957fc197fc0a642c120ff219e96
Javi Merino <merino.jav@gmail.com>
2024-03-06 22:06:39 +0000

    fd-send-recv: Add a package to fix building fd-send-recv with ocaml prior to 5

--- pkgs/fd-send-recv/default.nix
+++ pkgs/fd-send-recv/default.nix
@@ -18,10 +18,12 @@ ocamlPackages.buildDunePackage rec {
     hash = "sha256-gcChczm5uYLIgAiEOM23VdGbhyieu6CQ19B1lmwMajQ=";
   };
 
+  patches = [
+    ./0001-fd_send_recv_stubs-fix-build-with-ocaml-earlier-than.patch
+  ];
   doCheck = true;
 
   meta = with lib; {
-    broken = true;
     homepage = "https://github.com/xapi-project/ocaml-fd-send-recv";
     description = "Bindings which allow Unix.file_descrs to be sent and received over Unix domain sockets";
     license = licenses.lgpl21Only;
