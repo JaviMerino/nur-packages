44aeea51d162ad7ead0d4ec33929e590d575251c
Javi Merino <merino.jav@gmail.com>
2025-10-25 13:29:03 +0100

    say: update to latest version

--- pkgs/say/default.nix
+++ pkgs/say/default.nix
@@ -10,8 +10,8 @@ let
   say_repo = fetchFromGitHub {
     owner = "JaviMerino";
     repo = "say-tts";
-    rev = "0ebb75cdf2c299319c56d143dd3698704941ea71";
-    hash = "sha256-36D3gRg9j7UKGLcUr7vZ13F7N1pE9CLOS8kMPSqeJ9o=";
+    rev = "71ab9aafc721c813459f2e93660c6485aa60c921";
+    hash = "sha256-bjO+SW3HWEzis8iB74ogkaWI1+qfa/OtfobTKaD1xhQ=";
   };
   say_text = builtins.readFile "${say_repo}/say";
 in
